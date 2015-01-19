3abfc0598f9f53fae7022621ff33863a53d06550
ashleycoker <ashley.coker@prowebsofwtare.net>
2015-01-19 11:51:42 +0000

    Argument

--- copyImages.sh
+++ copyImages.sh
@@ -1,4 +1,4 @@
-inotifywait -m %1 -e create -e moved_to |
+inotifywait -m $1 -e create -e moved_to |
     while read path action file; do
         echo "The file '$file' appeared in directory '$path' via '$action'"
         # do something with the file
