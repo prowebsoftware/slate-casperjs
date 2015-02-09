10910f10af6fbfc8f3576926b8a0d548fd78bd9c
ashleycoker <ashley.coker@prowebsofwtare.net>
2015-02-09 20:07:37 +0000

    Jenkins confg.xml

--- copyImages.sh
+++ copyImages.sh
@@ -2,13 +2,15 @@ inotifywait -m -r $1 -e create -e moved_to |
     while read path action file; do
         echo "The file '$file' appeared in directory '$path' via '$action'"
         # do something with the file
+
         # scp $path$file slate@prowebsoftware.redirectme.net:~/Documents/captures/$file
+
         if [[ $file =~ ^-?[0-9]+$ ]]
         then
-          echo "${file} is an integer"
-          scp $path$file slate@prowebsoftware.redirectme.net:~/Documents/captures/$file/
+            echo "${file} is an integer"
+            scp $path$file slate@prowebsoftware.redirectme.net:~/Documents/captures/${path%%[^0-9]*}/$file/
         else
             echo "${file} is not an integer"
-          scp $path$file slate@prowebsoftware.redirectme.net:~/Documents/captures
+            scp $path$file slate@prowebsoftware.redirectme.net:~/Documents/captures
         fi
     done
