ca7ec59b7e1685a308f7b0a1defdf1ceb1b91e2c
ashleycoker <ashley.coker@prowebsofwtare.net>
2015-02-09 21:37:17 +0000

    Copy images

--- copyImages.sh
+++ copyImages.sh
@@ -8,9 +8,11 @@ inotifywait -m -r $1 -e create -e moved_to |
         if [[ $file =~ ^-?[0-9]+$ ]]
         then
             echo "${file} is an integer"
-            scp $path$file slate@prowebsoftware.redirectme.net:~/Documents/captures/${path%%[^0-9]*}/$file/
+            scp $path$file slate@prowebsoftware.redirectme.net:~/Documents/captures/$file/
         else
             echo "${file} is not an integer"
-            scp $path$file slate@prowebsoftware.redirectme.net:~/Documents/captures
+            dirname=$(echo $path | cut -d"/" -f7);
+            echo "${dirname} is the dirname to copy to"
+            scp $path$file slate@prowebsoftware.redirectme.net:~/Documents/captures/$dirname/
         fi
     done
