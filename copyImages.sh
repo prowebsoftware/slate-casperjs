7f62646399817f472ae96ad4ce867d847e93a71b
ashleycoker <ashley.coker@prowebsofwtare.net>
2015-02-09 22:13:10 +0000

    Copy images

--- copyImages.sh
+++ copyImages.sh
@@ -5,15 +5,16 @@ inotifywait -m -r $1 -e create -e moved_to |
 
         # scp $path$file slate@prowebsoftware.redirectme.net:~/Documents/captures/$file
 
+        # check if $file is an integer
         if [[ $file =~ ^-?[0-9]+$ ]]
         then
-            echo "${file} is an integer"
+            echo "${file} is our counter"
             # scp $path$file slate@prowebsoftware.redirectme.net:~/Documents/captures/$file/
             # ssh slate@prowebsoftware.redirectme.net "cd ~/Documents/captures/; mkdir ${file}"
         else
-            echo "${file} is not an integer"
+            echo "${file} is our file name (not our counter)"
             dirname=$(echo $path | cut -d"/" -f7);
-            echo "${dirname} is the dirname to copy to"
+            # echo "${dirname} is the dirname to copy to"
             # scp $path$file slate@prowebsoftware.redirectme.net:~/Documents/captures/$dirname/
             rsync -ave ssh $path$file slate@prowebsoftware.redirectme.net:~/Documents/captures/$dirname/
         fi
