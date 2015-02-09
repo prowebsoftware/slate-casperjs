fa0af86fc4436cdee0c4101fbf965f2d5c717245
ashleycoker <ashley.coker@prowebsofwtare.net>
2015-02-09 19:58:34 +0000

    Jenkins confg.xml

--- copyImages.sh
+++ copyImages.sh
@@ -3,7 +3,7 @@ inotifywait -m -r $1 -e create -e moved_to |
         echo "The file '$file' appeared in directory '$path' via '$action'"
         # do something with the file
         # scp $path$file slate@prowebsoftware.redirectme.net:~/Documents/captures/$file
-        if [ -z "${file##+([0-9])}" ]
+        if [[ $file =~ ^-?[0-9]+$ ]]
         then
           echo "${file} is an integer"
           scp $path$file slate@prowebsoftware.redirectme.net:~/Documents/captures/$file/
