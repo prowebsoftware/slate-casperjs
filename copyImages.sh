46c8e2c66ea0cf093de62ad039afbd59628ba5f8
ashleycoker <ashley.coker@prowebsofwtare.net>
2015-02-09 19:47:31 +0000

    Jenkins confg.xml

--- copyImages.sh
+++ copyImages.sh
@@ -2,5 +2,5 @@ inotifywait -m -r $1 -e create -e moved_to |
     while read path action file; do
         echo "The file '$file' appeared in directory '$path' via '$action'"
         # do something with the file
-        scp $path$file slate@prowebsoftware.redirectme.net:~/Documents/captures
+        scp $path$file slate@prowebsoftware.redirectme.net:~/Documents/captures/$file
     done
