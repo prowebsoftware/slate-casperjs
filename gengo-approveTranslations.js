965271b4f6002bc8df9ddaf1b44f1a3ba19fb345
ashleycoker <ashley.coker@prowebsoftware.net>
2015-03-10 12:36:26 +0000

    Update gengo-approveTranslations.js

--- gengo-approveTranslations.js
+++ gengo-approveTranslations.js
@@ -81,7 +81,7 @@ casper.start('https://sandbox.gengo.com/auth/form/login/', function(){
 
 casper.waitForResource("https://sandbox.gengo.com/auth/form/login/",function() {
     //this.fill('form[action="https://sandbox.gengo.com/auth/login/"]', {
-    this.fill('form:first-of-type', {
+    this.fill('//*[@id="login"]/form', {
         login_email : 'ashley.coker@prowebsoftware.net',
         login_password: 'rVr#fJ9(6x'
     }, true);
